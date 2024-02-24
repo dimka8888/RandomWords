1e557806edd835e096df09b56333b11027b98090
Dmitry-Ivanchikov <dmitry.ivanchikov@gmail.com>
2024-02-25 02:19:49 +0300

    add words

--- src/main/java/PageRandomWords.java
+++ src/main/java/PageRandomWords.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class PageRandomWords {
+public class PageRandomWords {
+
+    public static void main(String[] args) {
+        String[] wordListOne = {"Счастье", "Правильный", "грустный", "Мама", " Папа", "Дом", "Солнце", " Море", " Любовь", "Счастье", "Улыбка", "Дружба", " Осень"};
+        String[] wordListYwo = {"Рассвет", "Закат", "Весна", "Зима", "Лето", "Кошка", "Собака", "Дождь", "Ветер", "Цветы", "Дерево"};
+        String[] wordListThree = {"Птица", "Радость", "Праздник", "Музыка", "Танец", "Фильм", "Книга", "Спорт", "Путешествие"};
+
+        int oneLength = wordListOne.length;
+        int twolength = wordListYwo.length;
+        int threelength = wordListThree.length;
+
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twolength);
+        int rand3 = (int) (Math.random() * threelength);
+
+        String phrase = wordListOne[rand1] + "   " + wordListThree[rand2] + "    " + wordListYwo[rand3];
+        System.out.println("Жизнь зто-" + phrase);
+    }
 }
--- src/main/java/training/ivanchikov/Main.java
+++ src/main/java/training/ivanchikov/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package training.ivanchikov;
 
 public class Main {
     public static void main(String[] args) {
